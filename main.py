d6506d8c22fc949f034d2d5935a27c0438cd4522
dsump <dsumpmann@gmail.com>
2022-08-05 12:46:08 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import turtle
+
+screen = turtle.Screen()
+screen.title("US States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+screen.exitonclick()
